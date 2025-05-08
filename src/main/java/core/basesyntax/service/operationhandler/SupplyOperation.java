b23ca76ff1344e8622fb24d278c2faf568ca2b6c
Ivanov <ivanshevchenko181@gmail.com>
2025-05-08 13:29:53 +0300

    created the project, solution is sufficient

---
+++ src/main/java/core/basesyntax/service/operationhandler/SupplyOperation.java
@@ -0,0 +1,13 @@
+package core.basesyntax.service.operationhandler;
+
+import static core.basesyntax.db.ShopDataBase.shopData;
+
+import core.basesyntax.service.FruitTransaction;
+
+public class SupplyOperation implements OperationHandler {
+    @Override
+    public void process(FruitTransaction fruitTransaction) {
+        int oldQuantity = shopData.get(fruitTransaction.getFruit());
+        shopData.put(fruitTransaction.getFruit(), fruitTransaction.getQuantity() + oldQuantity);
+    }
+}
