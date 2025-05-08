71287cca43278a12c551a34f416016ffe74909e1
Ivanov <ivanshevchenko181@gmail.com>
2025-05-08 14:26:21 +0300

    new file writer Impl solution

--- src/main/java/core/basesyntax/dao/FileWriterImpl.java
+++ src/main/java/core/basesyntax/dao/FileWriterImpl.java
@@ -1,16 +1,23 @@
 package core.basesyntax.dao;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class FileWriterImpl implements FruitFileWriter {
 
     @Override
     public void write(List<String> report, String fileName) {
-        try {
-            Files.write(Path.of(fileName),report);
+        Path filePath = Path.of(fileName);
+        try (BufferedWriter writer = Files.newBufferedWriter(filePath,
+                StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
+            for (String line : report) {
+                writer.write(line);
+                writer.newLine();
+            }
         } catch (IOException e) {
             throw new RuntimeException("Can't write into the file by path: " + fileName, e);
         }
